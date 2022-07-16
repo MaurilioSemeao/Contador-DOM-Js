28b63855f8b00964fb4682d2ee760d157682159e
MaurilioSemeao <106936169+MaurilioSemeao@users.noreply.github.com>
2022-07-16 15:27:51 -0300

    mudança de cor
    implementa alteração de cor para números positivos e negativos

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,18 @@ var currentNumber = 0;
 function increment(){
     currentNumber = currentNumber + 1;
     currentNumberWrapper.innerHTML = currentNumber;
-
+    if(currentNumber>=0){
+        currentNumberWrapper.style.color = 'blue';
+    }
 
 }
 
 function decrement(){
     currentNumber = currentNumber - 1;
     currentNumberWrapper.innerHTML = currentNumber;
+    if(currentNumber < 0){
+        currentNumberWrapper.style.color = 'red';
+    }
 }
 
 
